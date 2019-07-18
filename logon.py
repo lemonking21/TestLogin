b0f84264a1347eb26d12a4227cf4cddbed2db3c6
lemonking21 <36713830+lemonking21@users.noreply.github.com>
2019-07-18 17:25:15 -0400

    Add files via upload

--- logon.py
+++ logon.py
@@ -1,5 +1,18 @@
+#Created by Kyle Krug 
+#created on 7/18/2019
+#LASTED updated on 7/18/2019
+#created to show how a basic log in can be created 
+#PLEASE NOTE that this is not a secure login system
+# the passwords need to be encrypted, salted and hashed 
+#DO NOT USE FOR ANY SOFTWARE/ PROGRAM BEFORE SECURING IT!!!!!
+
 import json
 import re
+from saltedmeat import prepare_the, inspect_the
+import os
+
+
+
 
 accounts = []
 password =''
@@ -22,7 +35,7 @@ def get_username():
     else:
         get_username()
     check_user()
-
+    
 def set_username(name):
     global username
     username = name
@@ -36,21 +49,21 @@ def get_password():
 
 def set_password(passw):
     global password
-    password = passw
+    password = prepare_the(passw)
 
 def record_setup():
     global username, password
     account = { 'username': username, 'password': password, 'accountid' : len(accounts)}
     return account
 
-def write_write_to_file():
+def write_write_to_file(mode):
     account = record_setup()
-    with open('jsontest.json', 'a') as outf:
+    print('setup done!')
+    with open('jsontest.json', mode) as outf:
         json.dump(account,outf)
         outf.write('\n')
     print('New account created!!')
     opeing_menu()
-
 
 def readfromFile():
     global accounts
@@ -65,7 +78,10 @@ def loggedin():
     check_username(usernameL, passwordL)
 
 def check_password(i, passwordL):
-    if accounts[i]['password']==passwordL:
+    fpassword = accounts[i]['password']
+    passworda = inspect_the(passwordL,fpassword )
+    print(passworda)
+    if fpassword == passworda:
         return 'Good'
 
 def check_username(usernameL,passwordL):
@@ -80,7 +96,6 @@ def check_username(usernameL,passwordL):
                 print('Not the correct log in info!!!')
 
 def check_user():
-    if 'jsontest.json':
         readfromFile()
         account = record_setup()
         print(account)
@@ -91,14 +106,15 @@ def check_user():
                 print(accounts[record]['username'])
                 if account['password'] == accounts[record]['password']:
                     print('\n***************************************\nThis user already exists in the system\n***************************************\n')
-                    get_username()
-        write_write_to_file()           
-    else:
-        print('New Record')
+                    get_username()         
+            else:
+                print('New Record')
+        write_write_to_file('a')  
 
 def opeing_menu():
     i = 0
     while i != 3:
+        i = 0
         print("###########################################")
         print("######Welcome to my first Sign in program!!")
         print("##We store your username and password here!")
@@ -115,7 +131,14 @@ def opeing_menu():
             loggedin()
         if i == 3:
             print("Good Bye")
+        else:
+            opeing_menu()
+
 def main():
-    opeing_menu()
+    if os.path.exists('jsontest.json'):
+        opeing_menu()
+    else:
+        write_write_to_file('w')
+        main()
 
 main()
